ce52ec43cc40c0038f1d00b9060666c47044f9f6
Mohammad Abdullah <120156588+MohammadAbdullah5@users.noreply.github.com>
2024-12-01 13:20:21 +0500

    Fixed error of missing getCars route

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signup, signin, addVehicle, updateVehicle, deleteVehicle, viewAllVehicles, viewVehicleDetails, deleteAdmin } = require('../Controllers/userController');
+const { signup, signin, addVehicle, updateVehicle, deleteVehicle, viewAllVehicles, viewVehicleDetails, deleteAdmin, getCars } = require('../Controllers/userController');
 const authorize = require('../authorization')
 
 const router = express.Router();
@@ -12,5 +12,5 @@ router.delete('/delete-vehicle', authorize(['admin']), deleteVehicle);
 router.get('/view-vehicles', authorize(['admin']), viewAllVehicles);
 router.get('/vehicle-details/:licensePlate', authorize(['admin', 'owner']), viewVehicleDetails);
 router.delete('/delete-admin', authorize(['admin']), deleteAdmin);
-
+router.post('/getCars', authorize(['owner']), getCars);
 module.exports = router;
